59764a3a016aa7822c939878d51e99e79963bf14
Khaled Saad <elgendy1204@gmail.com>
2020-01-21 15:39:08 +0200

    Refactor login

---
+++ helpers/index.js
@@ -0,0 +1,23 @@
+export async function login() {
+    await page.goto(`${process.env.SITE_URL}/login`);
+
+    const navigationPromise = page.waitForNavigation();
+
+    await page.setViewport({ width: 1920, height: 948 });
+
+    await page.waitForSelector('.left-section-wrp > #login-form #input-signin-email');
+    await page.type('.left-section-wrp > #login-form #input-signin-email', process.env.USER_EMAIL);
+
+    await page.waitForSelector('.left-section-wrp > #login-form #input-signin-password');
+    await page.type(
+        '.left-section-wrp > #login-form #input-signin-password',
+        process.env.USER_PASSWORD
+    );
+
+    await page.waitForSelector(
+        '.content-card > .col-sm-6 > .left-section-wrp > #login-form > .btn'
+    );
+    await page.click('.content-card > .col-sm-6 > .left-section-wrp > #login-form > .btn');
+
+    await navigationPromise;
+}
--- test-cases/login.test.js
+++ test-cases/login.test.js
@@ -1,32 +1,13 @@
-describe('Login page', () => {
-    beforeAll(async () => {
-        await page.goto(`${process.env.SITE_URL}/login`);
-    });
+import { login } from '../helpers';
 
+describe('Login page', () => {
     it('Should be titled "Sign In | WUZZUF"', async () => {
+        await page.goto(`${process.env.SITE_URL}/login`);
         await expect(page.title()).resolves.toMatch('Sign In | WUZZUF');
     });
 
     it('Should sigin and go to explore', async () => {
-        const navigationPromise = page.waitForNavigation();
-
-        await page.setViewport({ width: 1920, height: 948 });
-
-        await page.waitForSelector('.left-section-wrp > #login-form #input-signin-email');
-        await page.type(
-            '.left-section-wrp > #login-form #input-signin-email',
-            process.env.USER_EMAIL
-        );
-
-        await page.waitForSelector('.left-section-wrp > #login-form #input-signin-password');
-        await page.type('.left-section-wrp > #login-form #input-signin-password', process.env.USER_PASSWORD);
-
-        await page.waitForSelector(
-            '.content-card > .col-sm-6 > .left-section-wrp > #login-form > .btn'
-        );
-        await page.click('.content-card > .col-sm-6 > .left-section-wrp > #login-form > .btn');
-
-        await navigationPromise;
+        await login();
 
         expect(page.url()).toMatch(`${process.env.SITE_URL}/explore`);
     });
